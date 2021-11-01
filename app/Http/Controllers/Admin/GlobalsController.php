ffa7b5f2991842910fe52f538ff5361ab212cbd2
ye zhiming <mail@54zm.com>
2021-11-01 17:17:21 +0800

    系统管理全局管理优化

--- app/Http/Controllers/Admin/GlobalsController.php
+++ app/Http/Controllers/Admin/GlobalsController.php
@@ -24,17 +24,13 @@ class GlobalsController extends Controller
     public function set(GlobalSet $request): array
     {
         $site_name            = $request->get('site_name');          // 网站名称
-        $download_link        = $request->get('download_link');      // 下载地址
-        $kf_address           = $request->get('kf_address');         // 客服地址
+        $download_link        = $request->get('download_link');      // APP下载地址
+        $kf_address           = $request->get('kf_address');         // 客服连接地址
         $invitation_code      = $request->get('invitation_code');    // 邀请码开关 1：开启，0：关闭
-        $transaction_fees     = $request->get('transaction_fees');   // 交易手续费 请填写百分比%
         $withdrawal_fees      = $request->get('withdrawal_fees');    // 提现手续费 请填写百分比%
         $min_amount           = $request->get('min_amount');         // 最低提现额
-        $max_amount           = $request->get('max_amount');         // 最大提现额
-        $daily_cumulative     = $request->get('daily_cumulative');   // 日累计提现
-        $max_number           = $request->get('max_number');         // 最大提现次数
         $is_withdraw          = $request->get('is_withdraw');        // 是否开启提现  1：开启，0：关闭
-        $proxy_ratio          = $request->get('proxy_ratio');         // 代理红利比例  请填写百分比%
+        $increase_list        = $request->get('increase_list');        // 首页3+涨幅榜 例如：btcusdt,ethusdt,etcusdt
         $omni_wallet_address  = $request->get('omni_wallet_address'); // 钱包收款地址
         $erc20_wallet_address = $request->get('erc20_wallet_address');// 钱包收款地址
         $trc20_wallet_address = $request->get('trc20_wallet_address');// 钱包收款地址
@@ -43,14 +39,10 @@ class GlobalsController extends Controller
             'download_link'        => $download_link,
             'kf_address'           => $kf_address,
             'invitation_code'      => $invitation_code ? 1 : 0,
-            'transaction_fees'     => $transaction_fees,
             'withdrawal_fees'      => $withdrawal_fees,
             'min_amount'           => $min_amount,
-            'max_amount'           => $max_amount,
-            'daily_cumulative'     => $daily_cumulative,
-            'max_number'           => $max_number,
             'is_withdraw'          => $is_withdraw,
-            'proxy_ratio'          => $proxy_ratio,
+            'increase_list'        => $increase_list,
             'omni_wallet_address'  => $omni_wallet_address,
             'erc20_wallet_address' => $erc20_wallet_address,
             'trc20_wallet_address' => $trc20_wallet_address,
--- database/seeders/TableGlobals.php
+++ database/seeders/TableGlobals.php
@@ -18,24 +18,48 @@ class TableGlobals extends Seeder
 
         \DB::table('globals')->insert(array(
             array(
-                'fields' => 'site_name',
-                'value'  => '星风网络',
+                'fields' => 'site_name',    // 网站名称
+                'value'  => null,
             ),
             array(
-                'fields' => 'omni_wallet_address',
+                'fields' => 'download_link',    //  APP下载地址
+                'value'  => null,
+            ),
+            array(
+                'fields' => 'kf_address', // 客服连接地址
+                'value'  => null,
+            ),
+            array(
+                'fields' => 'invitation_code', // 邀请码开关 1：开启，0：关闭
+                'value'  => 1,
+            ),
+            array(
+                'fields' => 'withdrawal_fees', // 提现手续费 请填写百分比%
+                'value'  => 5,
+            ),
+            array(
+                'fields' => 'min_amount', // 最低提现额
+                'value'  => null,
+            ),
+            array(
+                'fields' => 'is_withdraw', // 是否开启提现  1：开启，0：关闭
+                'value'  => 1,
+            ),
+            array(
+                'fields' => 'increase_list', // 首页3+涨幅榜 例如：btcusdt,ethusdt,etcusdt
+                'value'  => "btcusdt,ethusdt,etcusdt",
+            ),
+            array(
+                'fields' => 'omni_wallet_address',  // OMNI协议钱包地址
                 'value'  => '1',
             ),
             array(
-                'fields' => 'erc20_wallet_address',
+                'fields' => 'erc20_wallet_address', // ERC20协议钱包地址
                 'value'  => '2',
             ),
             array(
-                'fields' => 'trc20_wallet_address',
+                'fields' => 'trc20_wallet_address', // TRC20协议钱包地址
                 'value'  => '3',
-            ),
-            array(
-                'fields' => 'withdrawal_fees',
-                'value'  => '5',
             ),
         ));
     }
