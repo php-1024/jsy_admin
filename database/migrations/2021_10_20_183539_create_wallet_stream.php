2d2a9b3f8a9ea35143f75d9f1ebd55fa7a409d92
ye zhiming <mail@54zm.com>
2021-11-05 19:29:07 +0800

    修改钱包流转类型

--- database/migrations/2021_10_20_183539_create_wallet_stream.php
+++ database/migrations/2021_10_20_183539_create_wallet_stream.php
@@ -23,8 +23,8 @@ class CreateWalletStream extends Migration
             $table->string('amount_before', 60)->comment('流转前的余额');
             $table->string('amount_after', 60)->comment('流转后的余额');
             $table->enum('way', [1, 2])->comment('流转方式 1 收入 2 支出');
-            $table->enum('type', [1, 2, 3, 4, 5, 6, 7, 8])->comment('流转类型 1 币币交易 2 永续合约 3 期权合约 4 申购交易 5 划转 6 充值 7 提现 8 冻结');
-            $table->enum('type_detail', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])->nullable()->comment('流转详细类型 0 提现 1 USDT充值  2 银行卡充值  3 币币交易手续费  4 永续合约手续费  5 期权合约手续费  6 币币账户划转到合约账户  7 合约账户划转到币币账户  8 申购冻结  9 币币交易  10 永续合约  11 期权合约');
+            $table->enum('type', [1, 2, 3, 4, 5, 6, 7, 8])->comment('流转类型 0 未知 1 充值 2 提现 3 划转 4 快捷买币 5 空投 6 现货 7 合约 8 期权 9 手续费');
+            $table->enum('type_detail', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])->nullable()->comment('流转详细类型 0 未知 1 USDT充值 2银行卡充值 3现货划转合约 4合约划转现货 5提现 6空投支出 7空投收入 8现货支出 9现货收入 10合约支出 11合约收入 12期权支出 13期权收入');
             $table->dateTime('created_at')->default(date('Y-m-d H:i:s'))->comment('创建时间');
             $table->dateTime('updated_at')->default(date('Y-m-d H:i:s'))->comment('更新时间');
             $table->dateTime('deleted_at')->nullable()->comment('删除时间，为 null 则是没删除');
