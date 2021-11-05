8972a01263e9ef8e380731a4cf69ed85816c6eb6
ye zhiming <mail@54zm.com>
2021-11-05 21:24:18 +0800

    issue_price

--- database/migrations/2021_10_11_113348_create_apply_buy.php
+++ database/migrations/2021_10_11_113348_create_apply_buy.php
@@ -22,7 +22,7 @@ class CreateApplyBuy extends Migration
             $table->integer('get_currency_num')->comment('购买数量');
             $table->integer('trading_pair_id')->nullable()->comment('交易对ID');
             $table->string('trading_pair_name')->nullable()->comment('交易对名称');
-            $table->string('ratio')->comment('购买比例');
+            $table->string('issue_price')->comment('发行价 1 = 多少个USDT');
             $table->dateTime('created_at')->default(date('Y-m-d H:i:s'))->comment('创建时间');
             $table->dateTime('updated_at')->default(date('Y-m-d H:i:s'))->comment('更新时间');
             $table->dateTime('deleted_at')->nullable()->comment('删除时间，为 null 则是没删除');
